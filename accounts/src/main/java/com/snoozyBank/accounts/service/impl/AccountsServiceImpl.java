0b5acc1359a82b5ed7f042be2f44606f6c8ed7f7
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2024-10-04 02:37:13 -0400

    added helper method createNewAccount

--- accounts/src/main/java/com/snoozyBank/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/snoozyBank/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,17 @@
 package com.snoozyBank.accounts.service.impl;
 
+import com.snoozyBank.accounts.constants.AccountsConstants;
 import com.snoozyBank.accounts.dto.CustomerDto;
+import com.snoozyBank.accounts.entity.Accounts;
+import com.snoozyBank.accounts.entity.Customer;
+import com.snoozyBank.accounts.mapper.CustomerMapper;
 import com.snoozyBank.accounts.repository.AccountsRepository;
 import com.snoozyBank.accounts.repository.CustomerRepository;
 import com.snoozyBank.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -19,6 +25,21 @@ private CustomerRepository customerRepository;
      */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(customer));
+    }
+
+    private Accounts createNewAccount(Customer customer)
+    {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
 
     }
 }
